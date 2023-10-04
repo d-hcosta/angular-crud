dd151b96208e652bdc2424cfe171b212fcac4495
d-hcosta <d.hcosta@proton.me>
2023-10-04 09:36:30 -0300

    fix: Linting

--- src/app/users/users-routing.module.ts
+++ src/app/users/users-routing.module.ts
@@ -6,18 +6,19 @@ import { ListComponent } from './list.component';
 import { AddEditComponent } from './add-edit.component';
 
 const routes: Routes = [
-    {
-        path: '', component: LayoutComponent,
-        children: [
-            { path: '', component: ListComponent },
-            { path: 'add', component: AddEditComponent },
-            { path: 'edit/:id', component: AddEditComponent }
-        ]
-    }
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      { path: '', component: ListComponent },
+      { path: 'add', component: AddEditComponent },
+      { path: 'edit/:id', component: AddEditComponent },
+    ],
+  },
 ];
 
 @NgModule({
-    imports: [RouterModule.forChild(routes)],
-    exports: [RouterModule]
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
 })
-export class UsersRoutingModule { }
+export class UsersRoutingModule {}
